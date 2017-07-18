a01518bb9e6166c3e751447cfdd7ac6c7ddb9eb3
chechelvitalii <chechelvitaliy@gmail.com>
2017-07-18 04:57:56 +0300

    url template

--- src/main/java/com/cv/aircraft/service/AircaraftService.java
+++ src/main/java/com/cv/aircraft/service/AircaraftService.java
@@ -8,8 +8,16 @@ public class AircaraftService {
 
     @Value("url.flight_in_zone")
     private String flightInZoneUrl;
-    //50 lat 30 lon
-    public void getAircraftsIn(Zone area) {
 
+    //50 lat 30 lon
+    public void getAircraftsIn(Zone zone) {
+        Zone.TopLeft topLeft = zone.getTopLeft();
+        Zone.BottomRight bottomRight = zone.getBottomRight();
+        String preparedUrl = prepareUrl(topLeft, bottomRight);
     }
+
+    private String prepareUrl(Zone.TopLeft topLeft, Zone.BottomRight bottomRight) {
+        return flightInZoneUrl + topLeft.getLatitude() + "," + bottomRight.getLatitude() + "," + topLeft.getLongitude() + "," + bottomRight.getLongitude();
+    }
+
 }
